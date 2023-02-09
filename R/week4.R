9290bfc0a01c22122b26bdefed80f30a59bbb039
Vivien Lee <vivienlvv@gmail.com>
2023-02-09 14:09:13 -0600

    Completed steps 6 to 9

--- R/week4.R
+++ R/week4.R
@@ -1,3 +1,11 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
+
+# Data Import
+import_tbl = read_delim("../data/week4.dat", delim = "-", col_names = c("casenum", "parnum","stimver","datadate", "qs"), show_col_types = FALSE)
+glimpse(import_tbl)
+wide_tbl = separate(data = import_tbl, col = qs, into = paste0("q", 1:5), sep = " - ")
+
+
+
