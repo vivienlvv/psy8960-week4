473707452cdee7eb9da57ac53c04d2b0e1572a9c
Vivien Lee <vivienlvv@gmail.com>
2023-02-10 15:12:41 -0600

    Completed steps 10 to 14

--- R/week4.R
+++ R/week4.R
@@ -6,6 +6,12 @@ library(tidyverse)
 import_tbl = read_delim("../data/week4.dat", delim = "-", col_names = c("casenum", "parnum","stimver","datadate", "qs"), show_col_types = FALSE)
 glimpse(import_tbl)
 wide_tbl = separate(data = import_tbl, col = qs, into = paste0("q", 1:5), sep = " - ")
+wide_tbl[,5:9] = sapply(wide_tbl[,5:9], as.integer)
+wide_tbl$datadate = as.POSIXct(wide_tbl$datadate, format = "%b %d %Y, %T")
+wide_tbl[,5:9][wide_tbl[,5:9] == 0] = NA
 
 
 
+
+wide_tbl = wide_tbl[!is.na(wide_tbl$q2),]
+long_tbl = pivot_longer(wide_tbl, cols = starts_with("q"), names_to = "question", values_to = "response")
