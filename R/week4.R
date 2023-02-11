f3581adaca7751bd2ca120ebf1b7180929768828
Vivien Lee <vivienlvv@gmail.com>
2023-02-11 13:17:38 -0600

    removed paste0() to stick to base R

--- R/week4.R
+++ R/week4.R
@@ -5,7 +5,7 @@ library(tidyverse)
 # Data Import
 import_tbl = read_delim("../data/week4.dat", delim = "-", col_names = c("casenum", "parnum","stimver","datadate", "qs"), show_col_types = FALSE)
 glimpse(import_tbl)
-wide_tbl = separate(data = import_tbl, col = qs, into = paste0("q", 1:5), sep = " - ")
+wide_tbl = separate(data = import_tbl, col = qs, into = c("q1","q2","q3","q4","q5"), sep = " - ")
 wide_tbl[,5:9] = sapply(wide_tbl[,5:9], as.integer)
 wide_tbl$datadate = as.POSIXct(wide_tbl$datadate, format = "%b %d %Y, %T")
 wide_tbl[,5:9][wide_tbl[,5:9] == 0] = NA
