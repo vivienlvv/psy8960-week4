cdc4bd7cd548c2d38251c9e8e62a4f8dcae6372e
Vivien Lee <vivienlvv@gmail.com>
2023-02-13 12:28:24 -0600

    Class demo notes update

--- R/week4.R
+++ R/week4.R
@@ -15,3 +15,14 @@ wide_tbl[,5:9][wide_tbl[,5:9] == 0] = NA
 
 wide_tbl = wide_tbl[!is.na(wide_tbl$q2),]
 long_tbl = pivot_longer(wide_tbl, cols = starts_with("q"), names_to = "question", values_to = "response")
+
+
+## Notes from class demo: 02/13/2023
+## Anything in the base R package is fair game now 
+## Line 8: now can use paste0("q",1:5)
+## Line 9: wide_tbl[5:9] will also work because dataframes are lists 
+## Example of getting only integer columns wide_tbl[,sapply(wide_tbl, is.integer)]
+## I was originally thinking sapply(wide_tbl[,5:9], function(x) ifelse(x == 0, NA, x))
+## Another useful function is na_if(x = vector to modify, y = value to replace with NA)
+## replace_na(data, replace, ...)
+## Line 16: drop_na(wide_tbl, q2)
